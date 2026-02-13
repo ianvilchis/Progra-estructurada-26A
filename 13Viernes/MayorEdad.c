fe448297000d15c456b2842e225fb2f34c460b07
ianvilchis <ian.vilchis1982@alumnos.udg.mx>
2026-02-13 08:56:09 -0600

    Create MayorEdad.c

---
+++ 13Viernes/MayorEdad.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A ;
+    printf ("Ingrese su edad");
+     scanf("%d",&A);
+    if (A >= 18) {
+        printf ("Es mayor de edad");
+        }
+        else{
+         printf ("Es menor de edad");
+        }
+    return 0;
+
+}
+
