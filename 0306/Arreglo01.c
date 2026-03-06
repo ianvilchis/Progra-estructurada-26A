5df4650d482c3f564791f59b80ea1635fdc673b0
ianvilchis <ian.vilchis1982@alumnos.udg.mx>
2026-03-06 07:54:18 -0600

    Fix printf statements for variable addresses

--- 0306/Arreglo01.c
+++ 0306/Arreglo01.c
@@ -5,7 +5,7 @@ int main()
 {
     float temp[24];
     float temp1, temp2;
-    printf("la direccion de temp1 es %d y la de temp2 es:%d",&temp1,&temp2);
-    printf("\nla direccion de temp[24] es %d con & y %d sin &",&temp,temp);
+    printf("la direccion de temp1 es %d \n y la de temp2 es:%d",&temp1,&temp2);
+    printf("\nla direccion de temp es %d con & y la de &temp[24] %d",temp,&temp[24]);
     return 0;
 }
