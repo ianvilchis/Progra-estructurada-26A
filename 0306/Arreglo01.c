21cb6c3765ff49017856044c4ba3ef7711455033
ianvilchis <ian.vilchis1982@alumnos.udg.mx>
2026-03-06 08:00:12 -0600

    Fix array index in address print statement

--- 0306/Arreglo01.c
+++ 0306/Arreglo01.c
@@ -6,6 +6,6 @@ int main()
     float temp[24];
     float temp1, temp2;
     printf("la direccion de temp1 es %d \n y la de temp2 es:%d",&temp1,&temp2);
-    printf("\nla direccion de temp es %d con & y la de &temp[24] %d",temp,&temp[24]);
+    printf("\nla direccion de temp es %d con & y la de &temp[24] %d",temp,&temp[23]);
     return 0;
 }
